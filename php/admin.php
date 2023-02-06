0469c86f40f23ae695db8325ee5c448d45b9ddf2
Charles-Caltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-02-06 11:08:26 +0100

    modif test

--- php/admin.php
+++ php/admin.php
@@ -8,7 +8,6 @@ if ($_SESSION['user'][0]['login'] != 'admin') {
     header("Location: index.php");
 }
 
-
 // On récupère tout le contenu de la table etudiants
 $sqlQuery = 'SELECT * FROM utilisateurs';
 $utilisateursStatement = $conn->query($sqlQuery);
